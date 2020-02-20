09b1821ddf00bb55fbe82b97196ece732ce9445b
Lena Zamnius <lena.zamnius@gmail.com>
2020-02-20 15:21:11 +0200

    add: upload solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
 // You should implement your task here.
-
+'use strict';
 module.exports = function towelSort (matrix) {
-  return [];
-}
+	if (!matrix || matrix.length === 0) return [];
+	let arr = [];
+	matrix.forEach((element, idx) => {
+		if (idx % 2 != 0) {
+			element.reverse();
+		}
+		element.forEach((val) => arr.push(val));
+	});
+	return arr;
+};
